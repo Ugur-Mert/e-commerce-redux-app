7f92ea6f938cf69b1aa1d65bb921da76f42a2cfa
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-05-12 05:22:34 +0300

    amount issues

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -1,46 +1,28 @@
-import React from "react";
+import { CartItem } from "./CartItem";
 import { useSelector } from "react-redux";
 import "./Cart.css";
 import { useDispatch } from "react-redux";
-import {
-  removeItem,
-  clearCart,
-  increase,
-  decrease,
-  calculateTotal,
-} from "../../features/card/cardSlice";
-import { FaTrash } from "react-icons/fa";
-import { IoMdArrowDropup, IoMdArrowDropdown } from "react-icons/io";
+import { clearCart } from "../../features/card/cardSlice";
 
 export const Cart = (id) => {
   const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.card);
 
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your bag</h2>
+          <h4 className="empty-cart">is currently empty.</h4>
+        </header>
+      </section>
+    );
+  }
+
   return (
     <div>
       {cartItems.map((item) => {
-        return (
-          <div className="cart" key={item.id}>
-            <img className="cart-image" src={item.image} alt={item.title} />
-            <h5 className="cart-title">{item.title}</h5>
-            <p>${item.price}</p>
-            <div>
-              <button onClick={() => dispatch(increase({ id }))}>
-                <IoMdArrowDropup />
-              </button>
-              <p className="amount">{amount}</p>
-              <button onClick={() => dispatch(decrease({ id }))}>
-                <IoMdArrowDropdown />
-              </button>
-            </div>
-            <p
-              className="remove-btn"
-              onClick={() => dispatch(removeItem(item.id))}
-            >
-              <FaTrash />
-            </p>
-          </div>
-        );
+        return <CartItem key={item.id} {...item} />;
       })}
       <hr />
       <div className="cart-footer">
---
+++ src/components/pages/CartItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { removeItem, increase, decrease } from "../../features/card/cardSlice";
+import { FaTrash } from "react-icons/fa";
+import { IoMdArrowDropup, IoMdArrowDropdown } from "react-icons/io";
+
+export const CartItem = ({ id, image, title, price, amount }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="cart" key={id}>
+      <img className="cart-image" src={image} alt={title} />
+      <h5 className="cart-title">{title}</h5>
+      <p>${price}</p>
+      <div>
+        <button onClick={() => dispatch(increase({ id }))}>
+          <IoMdArrowDropup />
+        </button>
+        <p className="amount">{amount}</p>
+        <button onClick={() => dispatch(decrease({ id }))}>
+          <IoMdArrowDropdown />
+        </button>
+      </div>
+      <p className="remove-btn" onClick={() => dispatch(removeItem(id))}>
+        <FaTrash />
+      </p>
+    </div>
+  );
+};
