a173b2283c68be47ece3351739a1d5fd1b74300c
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-05-12 05:35:55 +0300

    fixed fixed increase and decrease

--- src/components/pages/CartItem.js
+++ src/components/pages/CartItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { removeItem, increase, decrease } from "../../features/card/cardSlice";
 import { FaTrash } from "react-icons/fa";
 import { IoMdArrowDropup, IoMdArrowDropdown } from "react-icons/io";
 
-export const CartItem = ({ id, image, title, price, amount }) => {
+export const CartItem = ({ id, image, title, price }) => {
   const dispatch = useDispatch();
+  const { singleAmount } = useSelector((store) => store.card);
   return (
     <div className="cart" key={id}>
       <img className="cart-image" src={image} alt={title} />
@@ -15,7 +17,7 @@ export const CartItem = ({ id, image, title, price, amount }) => {
         <button onClick={() => dispatch(increase({ id }))}>
           <IoMdArrowDropup />
         </button>
-        <p className="amount">{amount}</p>
+        <p className="amount">{singleAmount}</p>
         <button onClick={() => dispatch(decrease({ id }))}>
           <IoMdArrowDropdown />
         </button>
--- src/features/card/cardSlice.js
+++ src/features/card/cardSlice.js
@@ -7,7 +7,8 @@ const url = "https://fakestoreapi.com/products";
 const initialState = {
   cardItems: [],
   cartItems: [],
-  amount: 1,
+  amount: 5,
+  singleAmount: 1,
   total: 0,
   isLoading: false,
 };
@@ -40,12 +41,12 @@ const cardSlice = createSlice({
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
     },
     increase: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id === payload.id);
-      cartItem.amount = cartItem.amount + 1;
+      state.cartItems.find((item) => item.id === payload.id);
+      state.singleAmount = state.singleAmount + 1;
     },
     decrease: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id === payload.id);
-      cartItem.amount = cartItem.amount - 1;
+      state.cartItems.find((item) => item.id === payload.id);
+      state.singleAmount = state.singleAmount - 1;
     },
     calculateTotal: (state) => {
       let amount = 0;
