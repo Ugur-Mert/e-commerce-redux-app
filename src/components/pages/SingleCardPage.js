106da34ff3e8cb197ecce963eff9821d67ba3006
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-05-17 16:15:20 +0300

    Single product page is done

--- src/components/pages/SingleCardPage.js
+++ src/components/pages/SingleCardPage.js
@@ -1,16 +1,54 @@
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "./SingleCardPage.css";
+import { FaStar } from "react-icons/fa";
+import { addItem } from "../../features/card/cardSlice";
+import { useDispatch } from "react-redux";
 
-const SingleCardPage = () => {
-  const { singlePageItems } = useSelector((store) => store.cardPage);
+export default function SingleCardPage() {
+  const params = useParams();
+  const [singleProduct, setSingleProduct] = useState([]);
+  const url = `https://fakestoreapi.com/products/${params.productId}`;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get(url).then((res) => setSingleProduct(res.data));
+  });
 
   return (
-    <div>
-      <h1>{singlePageItems.title}</h1>
-      <img src={singlePageItems.image} alt={singlePageItems.title} />
-      <p>{singlePageItems.description}</p>
-      <p>{singlePageItems.price}</p>
+    <div className="main-single-page">
+      <div className="single-page">
+        <img
+          className="single-image"
+          src={singleProduct.image}
+          alt={singleProduct.title}
+        />
+        <div>
+          <h1>{singleProduct.title}</h1>
+          <div className="single-page-detail">
+            {singleProduct.rating ? (
+              <h5>
+                <FaStar /> {singleProduct.rating.rate}
+              </h5>
+            ) : null}
+            {singleProduct.rating ? (
+              <h5>({singleProduct.rating.count})</h5>
+            ) : null}
+            <h5>Category: {singleProduct.category}</h5>
+          </div>
+          <p>$ {singleProduct.price}</p>
+          <p className="single-page-description">{singleProduct.description}</p>
+          <button
+            className="single-page-button"
+            onClick={() => {
+              dispatch(addItem(singleProduct.id));
+            }}
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
   );
-};
-
-export default SingleCardPage;
+}
