81c700ac4080e022de121389dcac494490caf862
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-06-21 20:18:53 +0300

    Page position

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Form } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 const ContactUs = () => {
   return (
@@ -9,24 +9,31 @@ const ContactUs = () => {
       <Row className="text-center mt-5">
         <p>CONTACT US</p>
       </Row>
-      <Col
-        style={{
-          display: "grid",
-          alignItems: "center",
-          minHeight: "100vh",
-        }}
-        className="mx-auto col-6 mb-5"
-      >
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Your Message</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-        </Form>
+      <Col className="mx-auto col-6 mb-5">
+        <Row
+          style={{
+            display: "grid",
+            alignItems: "center",
+            minHeight: "50vh",
+          }}
+        >
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Your Message</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Row>
+        <Row className=" mx-auto col-6">
+          <Button variant="outline-primary"> Send </Button>
+        </Row>
       </Col>
     </Container>
   );
