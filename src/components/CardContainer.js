9a5bb75aca3018f907a1b5abe7eff179fec815eb
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-06-21 20:14:16 +0300

    footer position

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -8,7 +8,12 @@ const CardContainer = () => {
 
   return (
     <Container>
-      <Row className="mx-auto text-center mt-5">
+      <Row
+        style={{
+          minHeight: "100vh",
+        }}
+        className="mx-auto text-center mt-5 mb-5"
+      >
         {cardItems.map((item) => {
           return (
             <Col key={item.id} className="m-3 cards text-center">
